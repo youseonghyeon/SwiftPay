ef7ef013491c52f4eab4c90d5ac37b254f3b260f
seonghyeon <dolla_@naver.com>
2024-09-01 19:08:39 +0900

    `AccountService`에 `createAccount` 함수 추가
    계정 생성 로직을 테스트 클래스에서 `AccountService` 클래스로 리팩토링했습니다.
    
    주요 변경 사항:
    - `createAccount` 함수 추가
    - 테스트 클래스에서 계정 생성 로직 제거 및 서비스로 이동

--- src/main/kotlin/com/swiftpay/service/AccountService.kt
+++ src/main/kotlin/com/swiftpay/service/AccountService.kt
@@ -1,9 +1,21 @@
 package com.swiftpay.service
 
+import com.swiftpay.entity.Account
 import com.swiftpay.repository.AccountRepository
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 @Service
 class AccountService(private val accountRepository: AccountRepository) {
+
+    fun createAccount(username: String, name: String, balance: BigDecimal): Account {
+        accountRepository.existsByUsername(username).let {
+            if (it) {
+                throw IllegalArgumentException("Account with username $username already exists")
+            }
+        }
+        val newAccount = Account(username = username, name = name, balance = balance, isAccountLocked = false)
+        return accountRepository.save(newAccount)
+    }
 
 }
--- src/test/kotlin/com/swiftpay/service/AccountServiceTest.kt
+++ src/test/kotlin/com/swiftpay/service/AccountServiceTest.kt
@@ -1,8 +1,7 @@
 package com.swiftpay.service
 
-import com.swiftpay.entity.Account
-import com.swiftpay.repository.AccountRepository
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,13 +17,12 @@ import kotlin.test.Test
 class AccountServiceTest {
 
     @Autowired
-    lateinit var accountRepository: AccountRepository
+    lateinit var accountService: AccountService
 
     @Test
     @DisplayName("유저 생성 테스트")
     fun `create account`() {
-        val account = createAccount("test", "Test User", BigDecimal(1000))
-        accountRepository.save(account)
+        val account = accountService.createAccount("test", "Test User", BigDecimal(1000))
         assertNotNull(account.id)
     }
 
@@ -32,20 +30,11 @@ class AccountServiceTest {
     @DisplayName("중복된 유저 생성 테스트")
     fun `create duplicate account`() {
         // 중복 사용자 생성 시도
-        createAccount("test", "Test User", BigDecimal(1000))
+        accountService.createAccount("test", "Test User", BigDecimal(1000))
         val exception = assertThrows<IllegalArgumentException> {
-            createAccount("test", "Test User", BigDecimal(1000))
+            accountService.createAccount("test", "Test User", BigDecimal(1000))
         }
         assertEquals("Account with username test already exists", exception.message)
     }
 
-    fun createAccount(username: String, name: String, balance: BigDecimal): Account {
-        accountRepository.existsByUsername(username).let {
-            if (it) {
-                throw IllegalArgumentException("Account with username $username already exists")
-            }
-        }
-        val newAccount = Account(username = username, name = name, balance = balance, isAccountLocked = false)
-        return accountRepository.save(newAccount)
-    }
 }
